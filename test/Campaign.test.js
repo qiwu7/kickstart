99417ceeb1a908cb4e928bc60661a3e41a6a49ed
qi.wu <qi.wu@coinbase.com>
2021-09-06 17:02:16 -0700

    More tests

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -34,4 +34,69 @@ describe("Campaigns", () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
+
+  it("marks caller as campaign manager", async() => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(manager, accounts[0]);
+  });
+
+  it("allows people to contribute money and marks them as approvers", async () => {
+    await campaign.methods.contribute().send({
+      value: "200",
+      from: accounts[1]
+    });
+
+    const isApprover = await campaign.methods.approvers(accounts[1]).call()
+    assert(isApprover);
+  });
+
+  it("requires a minimum contribution", async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: "5",
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it("allows a manager to make a payment request", async() => {
+    await campaign.methods
+      .createRequest("Buy batteries", "100", accounts[1])
+      .send({from: accounts[0], gas: "1000000", gasPrice: "5000000000"});
+
+    const request = await campaign.methods.requests(0).call();
+    assert.equal(request.description, "Buy batteries");
+    assert.equal(request.value, "100");
+    assert.equal(request.recipient, accounts[1]);
+  });
+
+  it("processes requests", async () => {
+    const manager = accounts[0];
+    const contributor = accounts[1];
+    const vendor = accounts[2];
+
+    await campaign.methods.contribute()
+      .send({ from: contributor, value: web3.utils.toWei("10", "ether") });
+
+    await campaign.methods
+      .createRequest("Buy batteries", web3.utils.toWei("5", "ether"), vendor)
+      .send({ from: manager, gas: "1000000", gasPrice: "5000000000" });
+
+    await campaign.methods.approveRequest(0)
+      .send({ from: contributor, gas: "1000000", gasPrice: "5000000000" });
+
+    const balanceBeforeDistribution = web3.utils.fromWei(
+      await web3.eth.getBalance(vendor), "ether");
+
+    await campaign.methods.finalizeRequest(0)
+      .send({ from: manager, gas: "1000000", gasPrice: "5000000000" });
+
+    const balanceAfterDistribution = web3.utils.fromWei(
+      await web3.eth.getBalance(vendor), "ether");
+
+    assert.equal(balanceAfterDistribution - balanceBeforeDistribution, 5);
+  });
 });
