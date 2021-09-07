fbff37ed820ab2a0fdee7df770dbb44b0deb891e
qi.wu <qi.wu@coinbase.com>
2021-09-06 22:55:11 -0700

    Rendering campaings

--- ethereum/ethers.js
+++ ethereum/ethers.js
@@ -6,9 +6,11 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   // We are in the browser and matamask is running.
   window.ethereum.request({ method: "eth_requestAccounts" });
   provider = new ethers.providers.Web3Provider(window.ethereum);
+  console.log("browser")
 } else {
   // We are on the server *OR* the user is not running metamask
   provider = new ethers.providers.JsonRpcProvider("https://ropsten.infura.io/v3/a0f543f3aee84be8b737f8a5f89ed9b8");
+  console.log("server")
 }
 
 export { ethers, provider };
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,37 @@
 import React from "react";
+import { Button, Card } from "semantic-ui-react";
 import factory from "../ethereum/factory";
+import 'semantic-ui-css/semantic.min.css'
 
 class CampaignIndex extends React.Component {
-  // Initialize state
-  state = {
-    address: ""
-  };
-
-  async componentDidMount() {
+  static async getInitialProps() {
     const campaigns = await factory.getDeployedCampaigns();
-    console.log(campaigns);
+
+    return { campaigns };
+  }
+
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      }
+    });
+
+    return <Card.Group items={items} />;
   }
 
   render() {
     return (
       <div>
         <h2>Campaigns Index!</h2>
+        {this.renderCampaigns()}
+        <Button 
+          content="Create Campaign"
+          icon="add"
+          primary
+        />
       </div>
     );
   }
