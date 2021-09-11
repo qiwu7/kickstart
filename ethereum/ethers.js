128b6439d73197627bdd59e10bf3699a21d28582
qi.wu <qi.wu@coinbase.com>
2021-09-10 23:31:22 -0700

    Form submission event handling

--- ethereum/ethers.js
+++ ethereum/ethers.js
@@ -1,16 +1,16 @@
 import { ethers } from "ethers";
 
 let provider;
+let signer;
 
 if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   // We are in the browser and matamask is running.
   window.ethereum.request({ method: "eth_requestAccounts" });
   provider = new ethers.providers.Web3Provider(window.ethereum);
-  console.log("browser")
+  signer = provider.getSigner();
 } else {
   // We are on the server *OR* the user is not running metamask
   provider = new ethers.providers.JsonRpcProvider("https://ropsten.infura.io/v3/a0f543f3aee84be8b737f8a5f89ed9b8");
-  console.log("server")
 }
 
-export { ethers, provider };
+export { ethers, provider, signer };
--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,12 +1,59 @@
 import React from "react";
+import {
+   Button, 
+   Form, 
+   Input,
+   Message,
+} from "semantic-ui-react";
 import Layout from "../../components/layout";
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
+import factory from "../../ethereum/factory";
+import { signer } from "../../ethereum/ethers";
 
 class CampaignNew extends React.Component {
+  state = {
+    minimumContribution: "",
+    errorMessage: "",
+    loading: false
+  };
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    this.setState({
+      errorMessage: "",
+      loading: true
+    });
+
+    try {
+      const factoryWithSigner = factory.connect(signer);
+      await factoryWithSigner.createCampaign(this.state.minimumContribution);
+    } catch (err) {
+      this.setState({errorMessage: err.message});
+    }
+
+    this.setState({loading: false});
+  };
+
   render() {
     return (
       <Layout>
-      <h1>New Campaign</h1>
+        <h3>Create a Campaign</h3>
+
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="wei" 
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={event => this.setState({minimumContribution: event.target.value})}
+            />
+          </Form.Field>
+
+          <Message error header="Oops!" content={this.state.errorMessage} />
+          <Button loading={this.state.loading} primary>Create!</Button>
+        </Form>
       </Layout>
     );
   }
