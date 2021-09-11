52f09a01079bf2d126b6722ddff455a89e53a45c
qi.wu <qi.wu@coinbase.com>
2021-09-10 22:51:49 -0700

    Add new compaign

---
+++ pages/campaigns/new.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../components/layout";
+import 'semantic-ui-css/semantic.min.css'
+
+class CampaignNew extends React.Component {
+  render() {
+    return (
+      <Layout>
+      <h1>New Campaign</h1>
+      </Layout>
+    );
+  }
+}
+
+export default CampaignNew;
